5781f4070c21b2d5a743fcb17efdda91b137b45a
OlegLeva <oleg.levitskiy@gmail.com>
2021-10-07 15:41:39 +0300

    try raise while&random.py

--- json_phonebook.py
+++ json_phonebook.py
@@ -20,3 +20,42 @@ try:
 except json.decoder.JSONDecodeError:
      phb = []
 f.close()
+
+data_json = [
+    {
+     'name': '',
+     'last_name': '',
+     'phone': '',
+     'city': ''
+     }
+]
+
+def add_dict(lst, dct):
+    name = input('Enter name: ')
+    last_name = input('Enter last_name: ')
+    phone = input('Enter phone: ')
+    city = input('Enter city: ')
+    new_dict = dct.copy()
+    new_dict['name'] = name
+    new_dict['last_name'] = last_name
+    new_dict['phone'] = phone
+    new_dict['city'] = city
+    return lst.append(new_dict)
+
+def search_name(name, lst):
+    for i in lst:
+        if i[name] == name:
+            print("Found person")
+            print(i)
+
+def search_last_name(last_name, lst):
+    for i in lst:
+        if i[last_name] == last_name:
+            print("Found person")
+            print(i)
+
+def search_phone(phone, lst):
+    for i in lst:
+        if i[phone] == phone:
+            print("Found person")
+            print(i)
