56d573ca22f193c53bf07f92a0ff1c9fa88253c7
OlegLeva <oleg.levitskiy@gmail.com>
2021-10-13 13:50:34 +0300

    shop

--- product_store.py
+++ product_store.py
@@ -4,11 +4,26 @@ class Product:
         self.name = name
         self.price = price
 
-class ProductStore(Product):
-    def __init__(self, type, name, price, amount=0):
-        super().__init__(type, name, price)
-        self.amount = amount
 
-    def add(self, price, amount):
-        self.price *= 1.3
-        self.amount += amount
+class ProductStore:
+    amount = 0
+    profit = 0
+    warehouse = []
+
+    def add(self, product, amount):
+        d = {}
+        d['product'] = product
+        d['amount'] = amount
+        product.price *= 1.3
+        self.warehouse.append(d)
+    print(warehouse)
+
+
+apple = Product('fruit', 'apple', 1.8)
+apple_store = ProductStore()
+apple_store.add(apple, 10)
+print(apple.price)
+print(apple_store.warehouse[0]['product'])
+
+
+
