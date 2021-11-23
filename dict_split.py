3936e62a32b020749e635124e6e32c735975e6fd
OlegLeva <oleg.levitskiy@gmail.com>
2021-11-23 22:28:38 +0200

    stack_max_numbr.py

--- dict_split.py
+++ dict_split.py
@@ -10,29 +10,29 @@ for word in sentence_list:
 print(sentence_dict)
 
 
-stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15
-}
-prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
-}
-total = {}
-for s in stock:
-    t = stock[s] * prices[s]
-    total[s] = t
+# stock = {
+#     "banana": 6,
+#     "apple": 0,
+#     "orange": 32,
+#     "pear": 15
+# }
+# prices = {
+#     "banana": 4,
+#     "apple": 2,
+#     "orange": 1.5,
+#     "pear": 3
+# }
+# total = {}
+# for s in stock:
+#     t = stock[s] * prices[s]
+#     total[s] = t
+#
+# print(total)
 
-print(total)
-
-i = [x for x in range(1, 11)]
-j = [y**2 for y in i]
-res = (i, j)
-print(res)
-
-rev_d = {v:k for k, v in total.items()}
-print(rev_d)
+# i = [x for x in range(1, 11)]
+# j = [y**2 for y in i]
+# res = (i, j)
+# print(res)
+#
+# rev_d = {v:k for k, v in total.items()}
+# print(rev_d)
--- stack_max_numbr.py
+++ stack_max_numbr.py
@@ -45,11 +45,40 @@ def coin():
     return randint(0, 1)
 
 
+
 def dice():
-    res = 0
-    for _ in range(6):
+    res = 1
+    for _ in range(5):
         res += coin()
+
     return res
 
 
 print(dice())
+
+
+
+# def test_dice(func):
+#     dict_test = {}
+#     for _ in range(100):
+#         v = func
+#         if dict_test[v] in dict_test: dict_test[v] += 1
+#         else: dict_test[v] = 1
+#         print(dict_test)
+#     return dict_test
+#
+# print(test_dice(dice()))
+
+# def test_dice(func):
+#     dict_test = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+#     for _ in range(1000):
+#         dict_test[func] += 1
+#     return dict_test
+#
+# print(test_dice(dice()))
+
+dict_test = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+
+print(2 in dict_test)
+
+
