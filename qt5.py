e452ca58f710e841f3ef54de62cba4ca06347812
OlegLeva <oleg.levitskiy@gmail.com>
2021-12-06 21:41:51 +0200

    QT

--- qt5.py
+++ qt5.py
@@ -1,23 +1,46 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication,
+                             QWidget,
+                             QLineEdit,
+                             QMainWindow,
+                             QHBoxLayout,
+                             QPushButton,
+                             QVBoxLayout,
+                             QGridLayout,
+                             QLabel)
 
 class MyWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWindowTitle('Третье окно')
-        helloWorldLabel = QLabel('<b><i>Hello</b></i>')
-        self.setCentralWidget(helloWorldLabel)
+        self.setGeometry(50, 70, 500, 500)
+        widget = QWidget()
+        kukuLabel = QLabel('<h1>Kuku</h1>')
+        editArea = QLineEdit('0')
+        mainButtom = QPushButton('Push')
+        mainButtom_2 = QPushButton('Second Push')
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(kukuLabel)
+        mainLayout.addWidget(editArea)
 
-def main_widget():
-    app = QApplication(sys.argv)
-    mainWidget = QWidget()
-    mainWidget.setWindowTitle('Моё второе окно')
-    # mainWidget.setFixedSize(500, 500)
-    mainWidget.setGeometry(50, 70, 500, 500)
-    helloWorldLabel = QLabel('<b><i>Hello</b></i>', parent=mainWidget)
-    helloWorldLabel.move(10, 25)
-    mainWidget.show()
-    app.exec()
+        mainLayout.addWidget(mainButtom)
+        mainLayout.addWidget(mainButtom_2)
+        widget.setLayout(mainLayout)
+        self.setCentralWidget(widget)
+
+#
+# def main_widget():
+#     app = QApplication(sys.argv)
+#     mainWidget = QWidget()
+#     mainWidget.setWindowTitle('Моё второе окно')
+#     # mainWidget.setFixedSize(500, 500)
+#     mainWidget.setGeometry(50, 70, 500, 500)
+#     helloWorldLabel = QLabel('<b><i>Hello</b></i>', parent=mainWidget)
+#     helloWorldLabel.move(10, 25)
+#     mainWidget.show()
+#     app.exec()
 
 def main_window():
     app = QApplication(sys.argv)
