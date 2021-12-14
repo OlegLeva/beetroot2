649d6daf8878c5649d6ff7b0486a83f734a00372
OlegLeva <oleg.levitskiy@gmail.com>
2021-12-14 20:56:48 +0200

    QT

--- Track_data/track_data_doc.py
+++ Track_data/track_data_doc.py
@@ -1,6 +1,5 @@
 from functools import partial
 import json
-import psycopg2
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (QApplication,
                              QWidget,
@@ -9,10 +8,10 @@ from PyQt5.QtWidgets import (QApplication,
                              QLineEdit,
                              QVBoxLayout,
                              QHBoxLayout,
-                             QGridLayout,
                              QPushButton,
                              QSizePolicy,
-                             QTextEdit)
+                             QTextEdit,
+                             QPlainTextEdit)
 
 import sys
 
@@ -39,6 +38,9 @@ class MyWindow(QMainWindow):
         self.btn_add = QPushButton('Добавить', self)
         self.btn_add.setFixedSize(150, 30)
         self.btn_add.setStyleSheet('background: rgb(223, 235, 233);')
+        self.btn_save = QPushButton('Сохранить', self)
+        self.btn_save.setFixedSize(150, 30)
+        self.btn_save.setStyleSheet('background: rgb(235, 223, 224);')
         self.head_Edit_Label = QLabel('Добавление информации')
         self.update_edit = QLineEdit('')
         self.update_edit.setFixedSize(600, 30)
@@ -47,10 +49,10 @@ class MyWindow(QMainWindow):
         self.update_edit.setPlaceholderText("Введите информацию")
         self.btn_2 = QPushButton('Добавить', self)
         self.btn_2.setFixedSize(150, 30)
-        self.btn_2.setStyleSheet('background: rgb(197, 237, 238);')
+        # self.btn_2.setStyleSheet('background: rgb(197, 237, 238);')
 
         self.answer = QTextEdit('')
-        self.answer.setPlaceholderText("ANSWER")
+        self.answer.setPlaceholderText("")
 
         mainLayout = QVBoxLayout()
         add_carLayout = QVBoxLayout()
@@ -59,13 +61,13 @@ class MyWindow(QMainWindow):
 
         add_carLayout.addWidget(self.add_track)
         add_carLayout.addWidget(self.btn_add)
+        add_carLayout.addWidget(self.btn_save)
 
         findLoyaut.addWidget(self.request)
         findLoyaut.addWidget(self.btn)
 
         editLoyaut.addWidget(self.update_edit)
         editLoyaut.addWidget(self.btn_2)
-
 
         mainLayout.addLayout(add_carLayout)
         mainLayout.addWidget(self.head_Request_Label)
@@ -85,41 +87,37 @@ class MyWindow(QMainWindow):
             self.car_json = []
         f.close()
 
-        patern_dict = {
-            'number_track': '',
-            'number_trailer': '',
-            'full_name' : '',
-            'phone' : '',
-            'custom_certificat': ''
+        self.patern_dict = {
+            'number': '',
+            'data': []
         }
-
-        #
-        # self.car_json = [
-        #     {
-        #         'number': '9313',
-        #         'data': ['BI9313EX', 'BI3866XF',
-        #                  'Терещенко Дмитрий Анатольевич',
-        #                  '+380980353438']
-        #     },
-        #     {
-        #         'number': '8576',
-        #         'data': ['BI8576EI', 'BI3844XF',
-        #                  'Возный Андрей Юрьевич',
-        #                  '+380679265219']
-        #     }
-        #
-        # ]
-
-        # with open("data.json", 'w') as file:
-        #     json.dump(self.car_json, file, indent=4, ensure_ascii=False)
 
         self.dump_json()
         self.btn.clicked.connect(partial(self.get_find_text))
-        self.btn_2.clicked.connect(partial(self.add_auto))
+        self.btn_add.clicked.connect(partial(self.add_auto))
+        self.btn_add.clicked.connect(self.add_track.clear)
+        self.btn_save.clicked.connect(partial(self.save_auto))
 
     def add_auto(self):
-        print(self.answer)
+        if self.patern_dict['number'] == '':
+            self.patern_dict['number'] = self.add_track.text()
+        elif self.patern_dict['number']:
+            self.patern_dict['data'].append(self.add_track.text())
+        text_print = f'<h3>{self.patern_dict["number"]}</h3>'
+        if self.patern_dict['data']:
+            for i in self.patern_dict['data']:
+                text_print += f"<h3>{i}\n<\h3>"
+        self.answer.setText(text_print)
 
+    def save_auto(self):
+        new_dict = self.patern_dict.copy()
+        self.car_json.append(new_dict)
+        self.patern_dict = {
+            'number': '',
+            'data': []
+        }
+        self.dump_json()
+        self.answer.setText("<h2>Сохранено</h2>")
 
     def dump_json(self):
         with open("data.json", 'w') as file:
@@ -130,18 +128,19 @@ class MyWindow(QMainWindow):
             if not self.request.text():
                 self.answer.setText('<h2>Введите номер авто или часть номера</h2>')
             elif self.request.text() in self.car_data['number']:
-                text_print = ''
+                text_print = f'<h3>{self.car_data["number"]}</h3>'
                 for i in self.car_data['data']:
                     text_print += f"<h3>{i}\n<\h3>"
                 self.answer.setText(text_print)
-
+                break
+            else:
+                self.answer.setText('<h2>Нет такого автомобиля</h2>')
 
     def add_text(self):
         print(self.car_json[0]['data'])
         for self.car_data in self.car_json:
             if self.car_data['number'] == self.request.text():
                 self.car_data['data'].append(self.update_edit.text())
-
 
 
 def main_window():
